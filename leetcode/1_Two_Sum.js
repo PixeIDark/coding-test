a176440cc47d8741d39f00ed758b3c6937414c61
Darkthos <kiki7951@naver.com>
2024-09-17 17:42:45 +0900

    1

--- leetcode/1_Two_Sum.js
+++ leetcode/1_Two_Sum.js
@@ -1,26 +1,77 @@
 // 인덱스 반환해라 합이 target되는
 
-const nums = [2, 7, 11, 15];
-const target = 9;
+const nums = [3, 3];
+const target = 6;
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 // 1. 오름차순 정렬되어있으니까 좌,우 인덱스를 인덱스를 비교한다.
 // 2. 좌우 인덱스를 비교했을 때 target이랑 같으면 return [index,index]
 // 3. target보다 좌우 인덱스 합이 작으면 좌 인덱스 값을 올린다.
+// 4. 음수일 경우, 양수로 바꿔서 계산? 아니다 섞일수도 있어
+//  [3,2,4] 6
+//  L    R
+// [2, 4]
 
-// [2,7,11,15] 22
-//    L    R
-// [0, 1]
+// 오름차순으로 정렬되서 오는줄알았는데 아님. sort써서 시간복잡도 높음. 기존 방식 폐기처분.
+// 51ms
+// var twoSum = function (nums, target) {
+//   let a = [];
+//   let n = nums.length - 1;
+//   let i = 0;
+//   let sortNums = nums.toSorted((a, b) => a - b);
+
+//   while (i <= n) {
+//     let sum = sortNums[i] + sortNums[n];
+
+//     if (sum < target) i++;
+//     if (sum > target) n--;
+//     if (sum === target)
+//       return [nums.indexOf(sortNums[i]), nums.lastIndexOf(sortNums[n])].sort(
+//         (a, b) => a - b
+//       );
+//   }
+// };
+
+// 1. Map에 key: n, value: num[n]을 넣고 target - num[n] 과 같은 value가 있는 key 찾기.
+// 2. 개소리임 key value 서로 바꿔야함.
+// nums = [-1, -2, -3, -4, -5]; target = -8;
+// n = -3, nums[i] = true, nums[n] =
+// map -7, 0
+
+// 생각보다 시간복잡도 높아서 폐기처분.
+// 55ms
+// var twoSum = function (nums, target) {
+//   let map = new Map();
+
+//   for (let i = 0; i < nums.length; i++) {
+//     let n = target - nums[i];
+//     if (map.has(nums[i])) {
+//       return [map.get(nums[i]), i];
+//     }
+//     map.set(n, i);
+//   }
+// };
+
+// 1. indexOf의 값은 -1 또는 1이상.
+// 2. -1일땐 false 1이상일시 true가 나오게 조건문 설정.
+// 3. +1 해주면 0되서 false뜸
+// 4. target/2 에 해당하는 값이 있으면 로직오류.
+// 5. 자기자신은 안되게 해야함
+// [3,3] 6
+
+// 회심의 역작.
+// 존나 느림
 var twoSum = function (nums, target) {
-  let arr = [];
-  let n = nums.length;
+  // let set = new Set([...nums])
 
+  for (let i = 0; i < nums.length; i++) {
+    let n = target - nums[i];
 
-  while() {
-
+    nums[i] = 0;
+    if (nums.indexOf(n) + 1) return [i, nums.lastIndexOf(n)];
   }
-  return arr;
 };
-
 console.log(twoSum(nums, target));
+
+// Map이 sort보다 시간복잡도가 높다..
--- leetcode/49_Group_Anagrams.js
+++ leetcode/49_Group_Anagrams.js
@@ -3,11 +3,58 @@
 const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
+// 1. 함수를 만들자 객체를 생성하여 str의 문자를 key 문자개수를 value로 저장하는
+// 2. 배열을 생성하자
+// 3. for문 or map을 돌려서 함수에 넣는다
+// 4. 객체들을 배열에 담아주자.
+// 5. while문을 통해 왼쪽과 오른쪽끝을 비교한다. 또 그안에서 for문으로 키 값 비교해야함
+// 6. false일시, 오른쪽은 왼쪽으로 한칸씩 이동
+// 7. true일시, 해당하는 index의 strs를 배열에 담고, 오른쪽은 왼쪽으로 한칸씩 이동
+// 8. 왼쪽과 오른쪽의 차이가 1이면 왼쪽에 해당하는 index의 strs를 배열에 담고 오른쪽 위치 초기화.
+// 5. 배열을 length를 기준으로 sort로 정렬
 var groupAnagrams = function (strs) {
-  let result = [];
+  const sortedStrs = strs.map((str) => {
+    return str.split("").sort().join("");
+  });
 
-  for (let i = 0; i <= strs.length; i++) {
-    if(strs[i])
+  let l = 0;
+  let r = strs.length - 1;
+  let arr = [];
+  let tempArr = [];
+  let validArr = [];
+
+  while (l < strs.length) {
+    if (l >= strs.length) break;
+
+    if (validArr.indexOf(l) !== -1) {
+      console.log(l, r, tempArr, validArr);
+      l++;
+      continue;
+    }
+
+    if (!tempArr.length) {
+      tempArr.push(strs[l]);
+      if (l === strs.length - 1) {
+        arr.push([strs[l]]);
+        break;
+      }
+    }
+
+    if (sortedStrs[l] === sortedStrs[r]) {
+      tempArr.push(strs[r]);
+      validArr.push(r);
+    }
+    r--;
+
+    if (l === r) {
+      l++;
+      r = strs.length - 1;
+      arr.push(tempArr.sort());
+      tempArr = [];
+    }
   }
+  return arr.sort((a, b) => a.length - b.length);
 };
 
+console.log(groupAnagrams(strs));
+// console.log(strs);
