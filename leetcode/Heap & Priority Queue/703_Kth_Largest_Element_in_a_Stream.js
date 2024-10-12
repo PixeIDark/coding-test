1c318a9502a8f880756d5dcdf3d0f6c56eb16d12
Darkthos <kiki7951@naver.com>
2024-10-13 02:40:25 +0900

    호

---
+++ leetcode/Heap & Priority Queue/703_Kth_Largest_Element_in_a_Stream.js
@@ -0,0 +1,28 @@
+// 우선 순위 큐 클래스 만들기.
+
+// Input:
+// ["KthLargest", "add", "add", "add", "add", "add"]
+// [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+
+// Output: [null, 4, 5, 5, 8, 8]
+
+// Explanation:
+
+// KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+// kthLargest.add(3); // return 4
+// kthLargest.add(5); // return 5
+// kthLargest.add(10); // return 5
+// kthLargest.add(9); // return 8
+// kthLargest.add(4); // return 8
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function (k, nums) {};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function (val) {};
