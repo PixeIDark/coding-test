12c735342f43e93d76bb8823def39999a9292d45
Darkthos <kiki7951@naver.com>
2024-10-29 19:39:26 +0900

    sa

--- leetcode/2-D DP/62_Unique_Paths.js
+++ leetcode/2-D DP/62_Unique_Paths.js
@@ -1,7 +1,7 @@
 // 왼쪽 끝 맨 밑까지 이동할 수 있는 경우의 수
 
 const m = 3,
-  n = 2;
+  n = 7;
 // Output: 3
 // Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
 // 1. Right -> Down -> Down
@@ -12,21 +12,14 @@ const m = 3,
 // grid만들고 이동한 지점을 표시해주는 시긍로 해보자
 // 이동한 위치까지의 경우의 수를 합쳐나가는 식으로해야함.
 var uniquePaths = function (m, n) {
-  // 방문 판독셔틀
-  const grid = Array.from({ length: m }, () => new Array(n).fill(0));
+  const grid = Array.from({ length: m }, () => new Array(n).fill(1));
 
-  // 경로까지의 경우의 수
-  const dp = Array.from({ length: m }, () => new Array(n).fill(0));
-  dp[0][0] = 1;
-
-  const bt = (y, x) => {
-    if (grid[y][x]) return;
-
-    grid[y][x] = 1;
-  };
-  bt(0, 0);
-
+  for (let y = 1; y < m; y++) {
+    for (let x = 1; x < n; x++) {
+      grid[y][x] = grid[y - 1][x] + grid[y][x - 1];
+    }
+  }
   return grid;
 };
 
-console.log(uniquePaths(m, n));
+console.table(uniquePaths(m, n));
