2f415a166dc93a54c6094863eaf079a62f376c30
Darkthos <kiki7951@naver.com>
2024-11-01 15:11:40 +0900

    s

--- leetcode/Daily/1671_Minimum_Number_of_Removals_to_Make_Mountain_Array.js
+++ leetcode/Daily/1671_Minimum_Number_of_Removals_to_Make_Mountain_Array.js
@@ -7,18 +7,32 @@ const nums = [4, 3, 2, 1, 1, 2, 3, 1]; // 1326
 // 짝수와 홀수 두 가지 경우로 나뉨.짝수일 경우 왼쪽 오른쪽 기준 정해줘야함.
 // 걍 nums 반갈죽해서 하나는 갈수록 커지고 하나는 갈수록 작아지면 되지않냐?
 var minimumMountainRemovals = function (nums) {
-  let result = 0;
-  let n = ~~((nums.length - 1) / 2); // length8일시 3, length7일시 3
+  const n = nums.length;
 
-  for (let i = 1; i <= n; i++) {
-    if (nums[i] <= nums[i - 1]) {
-      result++;
+  const l = new Array(n).fill(1);
+  const r = new Array(n).fill(1);
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        l[i] = Math.max(l[i], l[j] + 1);
+      }
     }
   }
 
-  for (let i = n + 1; i < nums.length - 1; i++) {
-    if (nums[i] <= nums[i + 1]) {
-      result++;
+  for (let i = n - 2; i >= 0; i--) {
+    for (let j = n - 1; j > i; j--) {
+      if (nums[i] > nums[j]) {
+        r[i] = Math.max(r[i], r[j] + 1);
+      }
+    }
+  }
+
+  let result = n;
+  for (let i = 1; i < n; i++) {
+    if (l[i] > 1 && r[i] > 1) {
+      let d = n - (l[i] + r[i] - 1);
+      result = Math.min(result, d);
     }
   }
 
