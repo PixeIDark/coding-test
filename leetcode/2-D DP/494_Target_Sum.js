b5e3d1e444727a4edcd68be9807bc01ef6a0b65e
Darkthos <kiki7951@naver.com>
2024-12-26 21:10:43 +0900

    5

--- leetcode/2-D DP/494_Target_Sum.js
+++ leetcode/2-D DP/494_Target_Sum.js
@@ -10,5 +10,29 @@ const nums = [1, 1, 1, 1, 1],
 // +1 + 1 + 1 - 1 + 1 = 3
 // +1 + 1 + 1 + 1 - 1 = 3
 
-//
-var findTargetSumWays = function (nums, target) {};
+// 음수와 양수 영역으로 나눠
+var findTargetSumWays = function (nums, target) {
+  const total = nums.reduce((a, b) => a + b, 0);
+
+  if (Math.abs(target) > total) return 0;
+
+  const dp = new Array(nums.length)
+    .fill(0)
+    .map(() => new Array(total * 2 + 1).fill(0));
+
+  dp[0][total + nums[0]] = 1;
+  dp[0][total - nums[0]] = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = -total; j <= total; j++) {
+      if (dp[i - 1][total + j] > 0) {
+        dp[i][total + j + nums[i]] += dp[i - 1][total + j];
+        dp[i][total + j - nums[i]] += dp[i - 1][total + j];
+      }
+    }
+  }
+
+  return dp;
+};
+
+console.table(findTargetSumWays(nums, target));
