c102d0b6cfaf18583c826b6daa1817ea3e9d36d7
PixeIDark <kiki7951@naver.com>
2025-02-23 19:55:23 +0900

    5

---
+++ leetcode/Daily/2025/2/23-889_Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.js
@@ -0,0 +1,39 @@
+// 전위 순회 및 후위 순회 참고해서 이진 트리 구축하기
+
+// Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+// Output: [1,2,3,4,5,6,7]
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+ function TreeNode(val, left, right) {
+     this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+      this.right = (right===undefined ? null : right)
+         }
+/**
+ * @param {number[]} preorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var constructFromPrePost = function(preorder, postorder) {
+    if (!preorder.length) return null
+
+    const root = new TreeNode(preorder[0])
+
+    if(preorder.length === 1) return root
+
+    const leftRootIdx = postorder.indexOf(preorder[1])
+    const leftSize = leftRootIdx + 1
+
+    root.left = constructFromPrePost(preorder.slice(1, leftSize + 1), postorder.slice(0,leftSize))
+    root.right = constructFromPrePost(preorder.slice(leftSize + 1), postorder.slice(leftSize, -1))
+
+    return root
+};
+
--- 특문제거.js
+++ 특문제거.js
@@ -1,6 +1,7 @@
-let newArr = JSON.parse(s.replace(/{/g, "[").replace(/}/g, "]"));
+let 아직안끝낫어  = JSON.parse(s.replace(/{/g, "[").replace(/}/g, "]"));
 JSON.stringify("h");
 
+// 아직안끝낫어
 // /{/g, '[' => {를 [ 로 대체한다.
 // JSON.parse        문자열 => 객체/배열
 // JSON.stringify 객체/배열 => 문자열
