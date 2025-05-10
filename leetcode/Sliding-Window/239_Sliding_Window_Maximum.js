8d7bc2493480c0f708ee29a470d38371936fc7ed
PixeIDark <kiki7951@naver.com>
2025-05-11 01:11:33 +0900

    34분

--- leetcode/Sliding-Window/239_Sliding_Window_Maximum.js
+++ leetcode/Sliding-Window/239_Sliding_Window_Maximum.js
@@ -1,43 +1,42 @@
-const nums = [1, 3, -1, -3, 5, 3, 6, 7];
-const k = 3;
+// 크기가 k 인 윈도우가 슬라이딩 할때 마다 배열 내의 최대 값을 배열에 담아서 반환
+
+const nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
 // Output: [3,3,5,5,6,7]
 // Explanation:
-// Window position                Max
+//     Window position                Max
 // ---------------               -----
-// [1  3  -1] -3  5  3  6  7       3
-//  1 [3  -1  -3] 5  3  6  7       3
-//  1  3 [-1  -3  5] 3  6  7       5
-//  1  3  -1 [-3  5  3] 6  7       5
-//  1  3  -1  -3 [5  3  6] 7       6
-//  1  3  -1  -3  5 [3  6  7]      7
+//     [1  3  -1] -3  5  3  6  7       3
+// 1 [3  -1  -3] 5  3  6  7       3
+// 1  3 [-1  -3  5] 3  6  7       5
+// 1  3  -1 [-3  5  3] 6  7       5
+// 1  3  -1  -3 [5  3  6] 7       6
+// 1  3  -1  -3  5 [3  6  7]      7
 
-// 양방향 큐로 조져보자
+// deque
+// 1. [0] 에 항상 최대 값이 존재해야함
+// 2. 인덱스 벗어나는 요소 자동 삭제
+// 3. 새 요소보다 작은 요소돌도 삭제
+// 34분
 var maxSlidingWindow = function (nums, k) {
-  const queue = [];
-  const result = [];
+    const result = []
+    const dequeue = []
+    const n = nums.length
 
-  for (let i = 0; i < k; i++) {
-    while (queue.length && nums[i] >= nums[queue[queue.length - 1]]) {
-      queue.pop();
-    }
-    queue.push(i);
-  }
-  result.push(nums[queue[0]]);
+    for (let i = 0; i < n; i++) {
+        while (dequeue.length && dequeue[0] <= i - k) {
+            dequeue.shift()
+        }
 
-  for (let i = k; i < nums.length; i++) {
-    if (queue[0] <= i - k) {
-      queue.shift();
-    }
+        while (dequeue.length && nums[dequeue[dequeue.length - 1]] < nums[i]) {
+            dequeue.pop()
+        }
 
-    while (queue.length && nums[i] >= nums[queue[queue.length - 1]]) {
-      queue.pop();
+        dequeue.push(i)
+
+        if (i >= k - 1) result.push(nums[dequeue[0]])
     }
 
-    queue.push(i);
-    result.push(nums[queue[0]]);
-  }
-
-  return result;
+    return result
 };
 
-console.log(maxSlidingWindow(nums, k));
+console.log(maxSlidingWindow(nums, k))
