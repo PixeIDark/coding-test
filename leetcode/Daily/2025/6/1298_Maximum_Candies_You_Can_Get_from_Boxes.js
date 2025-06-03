c0bfd00868c08285c85de443299c6bbde6acfe9c
PixeIDark <kiki7951@naver.com>
2025-06-03 23:55:38 +0900

    벽

---
+++ leetcode/Daily/2025/6/1298_Maximum_Candies_You_Can_Get_from_Boxes.js
@@ -0,0 +1,8 @@
+// 처음에 initalBoxes => status 0 이면 캔디 0개
+// 박스와 캔디가 나옴 => status 열려있으면 걍 바로염
+// 얻은 키 꾸준히 저장하고, 키를 가지고 있으면 status를 1로 바꿈
+// 키 없어서 접긑 못하는 상자 따로 저장해둠
+// 벽
+var maxCandies = function (status, candies, keys, containedBoxes, initialBoxes) {
+
+};
