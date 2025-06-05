b18f9b3bef99d436a1717c6bfff4c5dcd0815808
PixeIDark <kiki7951@naver.com>
2025-06-05 12:44:02 +0900

    8분

--- leetcode/2-D DP/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.js
+++ leetcode/2-D DP/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.js
@@ -6,30 +6,24 @@ const prices = [1, 2, 3, 0, 2];
 
 // 2차원 dp 만들어서 ㄱㄱ
 // 팔면 하루 쿨 있음
-// 벽느껴지네.. ㅋ
+// 복수
+// 8분
 var maxProfit = function (prices) {
-  const dp = new Array(prices.length).fill(0);
-  // dp 에 최대 이득을 저장
-  // dp-2의 값을 계승할 수 있음
+    let hold = -prices[0]
+    let sold = 0
+    let cool = 0
 
-  let amount = 0; // 이게 작아지는 시기오기 전에 팔아야하는게 기본 전제.
-  const bfs = (k) => {
-    const get = prices[k];
-    let amount;
-    for (let i = k + 1; i < prices.length; i++) {
-      if (amount < prices[i] || i === prices.length - 1) {
-        dp[i - 1] = amount + dp[i - 3];
-        continue;
-      }
-      amount = prices[i] - get;
+    for (let i = 1; i < prices.length; i++) {
+        const newHold = Math.max(hold, cool - prices[i])
+        const newSold = hold + prices[i]
+        const newCool = Math.max(cool, sold)
+
+        hold = newHold;
+        sold = newSold;
+        cool = newCool;
     }
-  };
 
-  for (let i = 0; i < prices.length; i++) {
-    bfs(i);
-  }
-
-  return dp;
+    return Math.max(sold, cool)
 };
 
 console.log(maxProfit(prices));
