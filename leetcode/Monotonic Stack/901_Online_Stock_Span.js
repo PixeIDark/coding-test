273083cbfa267ffdf1b7a6a16261509cd35ee692
PixeIDark <kiki7951@naver.com>
2025-06-05 12:31:09 +0900

    24분

--- leetcode/Monotonic Stack/901_Online_Stock_Span.js
+++ leetcode/Monotonic Stack/901_Online_Stock_Span.js
@@ -1,35 +1,36 @@
-// 클래스 잘 만들어서 주자
+// 자기자신 기본값 1 에서 현재날짜보다 엄격히 작은 날짜마다 카운팅
+// 스택에 넣는다 내림차순 기본으로
+// 마지막 값보다 큰게 나올 경우 위치 교환, count++
 
-// Input ["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
-//       [[], [100], [80], [60], [70], [60], [75], [85]]
-// Output [null, 1, 1, 1, 2, 1, 4, 6]
-// Explanation
-// StockSpanner stockSpanner = new StockSpanner();
-// stockSpanner.next(100); // return 1
-// stockSpanner.next(80);  // return 1
-// stockSpanner.next(60);  // return 1
-// stockSpanner.next(70);  // return 2
-// stockSpanner.next(60);  // return 1
-// stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
-// stockSpanner.next(85);  // return 6
-
-// 배열에 모든 값을 처넣고 next 할때는 잽싸게 모노토닉 스택 돌려서 length 값을 반환하자.
-var StockSpanner = function () {
-    this.stack = []
-    this.dp = []
+// 오름차순으로 하면 나보다 작은 얘가 나올 경우, 역시 위치 바꿈 ㅋ
+// 근데 바꾸고나서 현재의 인덱스 + 1 반환하면됨바로 length -1 인덱스보다 length -2 인덱스가 엄격히 작을 경우에만 바꿔야함
+// [100] => [100, 80] => [100, 80, 60] => [100, 80, 60,]vs 70
+// 엉터리 이론들임 연속적으로 주식이 낮거나 같냐를 보는거임
+// 24분
+var StockSpanner = function() {
+    this.stack = [];
+    this.map = new Map()
 };
 
 /**
  * @param {number} price
  * @return {number}
  */
-StockSpanner.prototype.next = function (price) {
-    this.stack.push(price)
-    this.dp.push(1)
+StockSpanner.prototype.next = function(price) {
+    const n = this.stack.length
+    let value = 1
 
-    if (this.stack.length >= 2 && this.stack[this.stack.length - 1] < this.stack[this.stack.length - 2]) this.dp[this.dp.length - 1] += this.dp[this.dp.length - 2]
+    while(this.stack[this.stack.length - 1] <= price) {
+        const key = this.stack.pop()
+        value += this.map.get(key)
+    }
+
+    this.stack.push(price)
+    this.map.set(price, value)
+
+    return value
 };
-// 이거 지금 절대 못품 ㅈㅈ
+
 /**
  * Your StockSpanner object will be instantiated and called as such:
  * var obj = new StockSpanner()
