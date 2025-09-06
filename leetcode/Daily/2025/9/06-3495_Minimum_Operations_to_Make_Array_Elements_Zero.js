3334c96a5be5c561a14c326e4a03ea3e153b0d1c
PixeIDark <kiki7951@naver.com>
2025-09-06 16:12:24 +0900

    벽

---
+++ leetcode/Daily/2025/9/06-3495_Minimum_Operations_to_Make_Array_Elements_Zero.js
@@ -0,0 +1,10 @@
+// [a, b] of queries, a 와 b 는 a ~ b 숫자들의 모음.
+// Math.floor(a / 4) ~ Math.floor(b / 4) 작업을 통해 0 만드는데 필요한 횟수
+// 100 => 1, 4 => 1
+// 1000 => 10, 8 => 2
+// 10000 => 100, 16 => 4
+// 2진법 기준, 작업 수행 시 n >> 2.
+// TODO: 벽, 구상조차 안됨
+var minOperations = function (queries) {
+
+};
