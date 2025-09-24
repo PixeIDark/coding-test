037d8a173ab12c778e7e97cd4b45dd17b7e8ac29
PixeIDark <kiki7951@naver.com>
2025-09-24 14:03:54 +0900

    벽

---
+++ leetcode/Daily/2025/9/24-166_Fraction_to_Recurring_Decimal.js
@@ -0,0 +1,8 @@
+// 분수를 순환소수로 전환. 반복되는 부분은 괄호로 묶어서 문자열로 반환, 분모 !== 0
+// TODO: 벽
+var fractionToDecimal = function (numerator, denominator) {
+    const num = numerator / denominator
+
+    // 0.2222123123123123123
+    // 순환이아닌 불규칙이 어디서 끝나는지만 알아도 괜찮
+};
