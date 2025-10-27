924433ea38c74b22df3b446f71653db7be8dd058
PixeIDark <kiki7951@naver.com>
2025-10-27 23:45:08 +0900

    5분

--- leetcode/Daily/2025/10/27-2125_Number_of_Laser_Beams_in_a_Bank.js
+++ leetcode/Daily/2025/10/27-2125_Number_of_Laser_Beams_in_a_Bank.js
@@ -2,29 +2,29 @@
 // bank[i] 에서 "1"의 개수를 mapping
 // bank.length - 2까지 진행하면서 서로 곱함
 // 5분
-var numberOfBeams = function(bank) {
-  const arr = []
+var numberOfBeams = function (bank) {
+  const arr = [];
 
-  for(const str of bank) {
-    let count = 0
+  for (const str of bank) {
+    let count = 0;
 
-    for(const char of str) {
-      if(char === "1") count++
+    for (const char of str) {
+      if (char === "1") count++;
     }
 
-    arr.push(count)
+    arr.push(count);
   }
 
-  let result = 0
+  let result = 0;
 
-  for(let i = 0; i < arr.length - 1; i++) {
-    if(arr[i + 1] === 0) {
-      arr[i + 1] = arr[i]
-      continue
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] === 0) {
+      arr[i + 1] = arr[i];
+      continue;
     }
 
-    result += arr[i] * arr[i + 1]
+    result += arr[i] * arr[i + 1];
   }
 
-  return result
+  return result;
 };
