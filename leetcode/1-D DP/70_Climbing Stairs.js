e83576f493b559b6fb4ba07d1e03cc9ef6e45371
PixeIDark <kiki7951@naver.com>
2025-12-30 00:35:15 +0900

    9분

--- leetcode/1-D DP/70_Climbing Stairs.js
+++ leetcode/1-D DP/70_Climbing Stairs.js
@@ -1,29 +1,16 @@
-// 꼭대기 경우의 수
-
-const n = 5;
-// Output: 3
-// Explanation: There are three ways to climb to the top.
-// 1. 1 step + 1 step + 1 step
-// 2. 1 step + 2 steps
-// 3. 2 steps + 1 step
-
-// 1칸 혹은 두칸 올라감
-// 동적특: dfs, bfs에서 원본의 자료구조를 토대로 값을 넣음.
-// 1 = 1, 2 = 2, 3 = 3, 4 = 5, 5 = 8
-// n[i] = n[i-1] + n[i-2]
+// [0, 1, 2, 3, 5, 8, 13]
+// dp[i] = dp[i - 1] + dp[i + 1]
+// 9분
 var climbStairs = function (n) {
-  if (n <= 2) return n;
+  if (n === 1) return 1;
 
-  let result = 0;
-  let onePrev = 2;
-  let twoPrev = 1;
+  const dp = Array(n + 1).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
 
-  for (let i = 3; i <= n; i++) {
-    result = onePrev + twoPrev;
-    twoPrev = onePrev;
-    onePrev = result;
+  for (let i = 3; i < dp.length; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
   }
-  return result;
-};
 
-console.log(climbStairs(n));
+  return dp.at(-1);
+};
