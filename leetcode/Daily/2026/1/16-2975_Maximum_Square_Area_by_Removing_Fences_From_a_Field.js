ea9da7604964d37850d9d970ac267276347de36d
PixeIDark <kiki7951@naver.com>
2026-01-16 21:12:38 +0900

    벽

---
+++ leetcode/Daily/2026/1/16-2975_Maximum_Square_Area_by_Removing_Fences_From_a_Field.js
@@ -0,0 +1,11 @@
+// 정사각형 최대 너비 반환 없으면 -1
+// m = 6, n = 7, hFences = [2], vFences = [4]
+// 3, 4, 5 가로 연속
+// 2, 3 | 5, 6 세로 연속
+// 가로 세로 연속길이가 같아야 정사각형
+// [false, false, true, false] 펜스 없으면 펄스
+// [2] 펜스 제거 [false, false, false, false]
+// TODO: 벽
+var maximizeSquareArea = function (m, n, hFences, vFences) {
+
+};
